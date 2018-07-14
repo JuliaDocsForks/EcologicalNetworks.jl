c672a46aa28bab001b45b8344012dbc67eaca4aa
Timothée Poisot <timothee.poisot@umontreal.ca>
2018-07-14 14:33:39 -0400

    :hand: tests

--- test.jl
+++ test.jl
@@ -7,5 +7,15 @@ using StatPlots
 
 N = nz_stream_foodweb()[5]
 
-
-bellman_ford(N)
+# Trophic species
+@progress for i in 1:(richness(N)-1)
+    s1 = species(N)[i]
+    for j in (i+1):richness(N)
+        s2 = species(N)[j]
+        same_in = N[s1,:] == N[s2,:]
+        same_out = N[:,s1] == N[:,s2]
+        if same_in  && same_out
+            println(s1, "\t", s2, "\t", same_in, "/", same_out, "\t", same_in&same_out)
+        end
+    end
+end
